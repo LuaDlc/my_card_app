cf586ee826e9536f09760afe496c2976907c2c0a
Luana Dias <luana.dias@colab.re>
2023-01-31 20:03:10 -0300

    app criado

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  //const MyApp({Key key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.amber,
         body: SafeArea(
             child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             CircleAvatar(
               backgroundImage: AssetImage('assets/image-icon.jpg'),
@@ -25,7 +26,66 @@ class MyApp extends StatelessWidget {
               'Luana Dias',
               style: TextStyle(
                   color: Colors.white, fontSize: 30, fontFamily: 'Regular'),
-            )
+            ),
+            const Text(
+              textAlign: TextAlign.center,
+              'DESENVOLVEDORA FLUTTER',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontFamily: 'Source Sans Pro',
+                  letterSpacing: 2.5),
+            ),
+            SizedBox(
+              width: 20,
+              height: 250.0,
+              child: Divider(
+                color: Colors.deepOrange.shade200,
+              ),
+            ),
+            const Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 25.0,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  size: 25,
+                  color: Colors.blue,
+                ),
+                title: Text(
+                  '+55 21 9 9809 9890',
+                  style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 20,
+                      fontFamily: 'Source Sans Pro',
+                      letterSpacing: 2.5),
+                ),
+              ),
+            ),
+            const Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    size: 25,
+                    color: Colors.blue,
+                  ),
+                  title: Text(
+                    'luadias@gmail.com',
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 20,
+                        fontFamily: 'Source Sans Pro',
+                        letterSpacing: 2.5),
+                  ),
+                ))
           ],
         )),
       ),
